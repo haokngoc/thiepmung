0f10de59d50034f1893b212f201ba7aadefa23bc
haokngoc <100467645+haokngoc@users.noreply.github.com>
2022-03-06 21:34:06 +0700

    Add files via upload
    creat card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'NgocXinhDep';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100006847457940';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc em không chỉ 8/3 mà cả đời phải vui vẻ nhé và phải luôn yêu bản thân mình thật nhiều đó!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
